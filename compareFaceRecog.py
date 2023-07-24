6da68f2e29e1a51aee0381bc66d4a67b51937e11
Tashanam Shahbaz <tashnamshahbaz72@gmail.com>
2023-07-24 13:43:07 +0500

    all to all compare

--- compareFaceRecog.py
+++ compareFaceRecog.py
@@ -14,77 +14,62 @@ firebase_app = initialize_app(
            })
 main_model = siamese_model()
 
-def compare_found_missing_faces_optimized(found_id):
+def compare_found_missing_faces_optimized():
     
     bucket = storage.bucket(app=firebase_app)
 
+    dic = {"ChildFound": [], "ChildMissing": []}
     
-    dic = {"ChildFound": [], "ChildMissing":[]}
-    
-    data_child_found = db.reference('ChildFoundInfo/'+found_id).get()
-    data_childern_missing=db.reference('ChildMissingInfo/').get()
+    data_childern_found = db.reference('ChildFoundInfo/').get()
+    data_childern_missing = db.reference('ChildMissingInfo/').get()
 
-    for imgPath in data_child_found['imagePath']: 
-        image_url_1 = bucket.blob(imgPath).generate_signed_url(
-                    timedelta(seconds=10000), method='GET')
-        
-        image_array=url_to_image(image_url_1)
-        if image_array is None:
-            continue
-        preprocess_image_array = preprocess_image(image_array)
-        if preprocess_image_array is None:
-            continue
-        
-        results = []
-        for child_missing_id, data_child_missing in data_childern_missing.items():
-            print("data_child_missing",data_child_missing)
-            for path in data_child_missing['imagePath']:
-                image_url_2 = bucket.blob(path).generate_signed_url(
-                            timedelta(seconds=10000), method='GET')
+    for child_found_id, data_child_found in data_childern_found.items():
+        if 'imagePath' in data_child_found:
+            for imgPath in data_child_found['imagePath']:
+                image_url_1 = bucket.blob(imgPath).generate_signed_url(timedelta(seconds=10000), method='GET')
+                
+                image_array = url_to_image(image_url_1)
+                if image_array is None:
+                    continue
+                preprocess_image_array = preprocess_image(image_array)
+                if preprocess_image_array is None:
+                    continue
+                
+                results = []
+                for child_missing_id, data_child_missing in data_childern_missing.items():
+                    print("data_child_missing",data_child_missing)
+                    if 'imagePath' in data_child_found:
+                        for path in data_child_missing['imagePath']:
+                            image_url_2 = bucket.blob(path).generate_signed_url(
+                                        timedelta(seconds=10000), method='GET')
 
-                pairs = saimese_pairs(image_url_2)
-                pairs[0][0, :, :, :] = preprocess_image_array
+                            pairs = saimese_pairs(image_url_2)
+                            pairs[0][0, :, :, :] = preprocess_image_array
 
-                dist = main_model.predict([pairs[0], pairs[1]])[0][0]
-                results.append((child_missing_id,image_url_2, dist))
-        
-        results=results.sort(key=lambda x: x[2])   
-        print(results)
+                            dist = main_model.predict([pairs[0], pairs[1]])[0][0]
+                            results.append((child_missing_id,image_url_2, dist))
+                    
+                    results.sort(key=lambda x: x[2])
 
-        data_child_found["images_path"] = [image_url_1]    
-        dic["ChildFound"].append({found_id: data_child_found}) 
+                    for i in range(3):
+                        missing_id = results[i][0]
+                        data_child_missing = data_childern_missing[missing_id]
+                        data_child_missing["images_path"] = [results[i][1]]
+                        dic["ChildMissing"].append({missing_id: data_child_missing})
 
-        data_child_found["images_path"] = [image_url_1]    
-        dic["ChildFound"].append({found_id: data_child_found})   
+                        data_child_found["images_path"] = [image_url_1]
+                        dic["ChildFound"].append({child_found_id: data_child_found})
 
-        data_child_found["images_path"] = [image_url_1]    
-        dic["ChildFound"].append({found_id: data_child_found})      
-
-
-        missing_id_1 = results[0][0]
-        data_child_missing = data_childern_missing[missing_id_1]
-        data_child_missing["images_path"] = [results[0][1]] 
-        dic["ChildMissing"].append({missing_id_1: data_child_missing})
-
-        missing_id_2 = results[1][0]
-        data_child_missing = data_childern_missing[missing_id_2]
-        data_child_missing["images_path"] = [results[0][1]] 
-        dic["ChildMissing"].append({missing_id_2: data_child_missing})
-
-        missing_id_3 = results[2][0]
-        data_child_missing = data_childern_missing[missing_id_3]
-        data_child_missing["images_path"] = [results[0][1]] 
-        dic["ChildMissing"].append({missing_id_3: data_child_missing})
-        
-    print("END",dic )
+    print("END", dic)
     return dic
 
 
-def process_video_and_upload_faces(found_id, remote_img_count):
+def process_video_and_upload_faces(child_id, remote_img_count):
 
     bucket = storage.bucket(app=firebase_app)
-    found_child_video_lst = list(bucket.list_blobs(
-        prefix=f"ChildFound/Video/{found_id}"))
+    id_type=child_id.split("-")[1]
+    child_video_lst = list(bucket.list_blobs(
+        prefix=f"Child{id_type}/Video/{child_id}"))
 
     temp_image_dir = "temp_Image"
     temp_video_dir = "temp_Video"
@@ -94,25 +79,25 @@ def process_video_and_upload_faces(found_id, remote_img_count):
     os.makedirs(temp_image_dir, exist_ok=True)
 
     try:
-        for video_count, video_name in enumerate(found_child_video_lst):
-            local_video_path = f"{temp_video_dir}/{found_id}_{video_count}.mp4"
+        for video_count, video_name in enumerate(child_video_lst):
+            local_video_path = f"{temp_video_dir}/{child_id}_{video_count}.mp4"
             remote_video_path = str(video_name).split(",")[1][1:]
 
             blob = bucket.blob(remote_video_path)
             blob.download_to_filename(local_video_path)
-            blob.delete()
+            # blob.delete()
 
             face_capture_from_video(local_video_path, temp_image_dir, video_count, remote_img_count)
 
         lst_images=os.listdir(temp_image_dir)
         
-        db_ref = db.reference('/ChildFoundInfo/'+found_id)
-        remote_image_lst=["ChildFound/Image/" + found_id + "/" + filename for filename in lst_images]
+        db_ref = db.reference(f"Child{id_type}Info/{child_id}")
+        remote_image_lst=["Child"+id_type+"/Image/" + child_id + "/" + filename for filename in lst_images]
         db_ref.child("imagePath").set(remote_image_lst)
         
         for i in lst_images:
             local_file_path = f"{temp_image_dir}/{i}"
-            remote_file_path = f"ChildFound/Image/{found_id}/{i}"
+            remote_file_path = f"Child{id_type}/Image/{child_id}/{i}"
 
             image_blob = bucket.blob(remote_file_path)
             image_blob.upload_from_filename(local_file_path)
--- main.py
+++ main.py
@@ -21,17 +21,15 @@ def view_compare_found_missing_faces_optimized():
     return result
 
 @app.route('/result_video', methods=['GET'])
-def view_process_video_upload_and_compare_faces():
+def view_process_video_upload_faces():
 
     # http://127.0.0.1:8000/result/?url_1=ali-found-ID&remote_img_count=5
-    child_found_id = request.args.get('url_1', '')
+    child_id = request.args.get('url_1', '')
     start= time.time()
-    result_video = process_video_and_upload_faces(child_found_id, remote_img_count = 5)
-    if result_video["Success"]:
-        result=compare_found_missing_faces_optimized(child_found_id)   
+    result_video = process_video_and_upload_faces(child_id, remote_img_count = 5)
     end= time.time()
     print("End-View-Video: TIME: ", end - start)
-    return result
+    return result_video
 
 if __name__ == '__main__':
     app.run()
